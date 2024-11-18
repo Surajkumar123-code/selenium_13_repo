0c2e1d87d78726fee6ee674ebe5effbd0ad327fd
Surajkumar123-code <mr.kumarsuraj1999@gmail.com>
2024-11-18 12:46:13 +0530

    synckClass added

---
+++ Automation_12_30/src/synck/SynckFork.java
@@ -0,0 +1,15 @@
+package synck;
+
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class SynckFork {
+	
+	@Test
+	public void synckClass() {
+		Reporter.log("synckClass added",true);
+	  
+	}
+
+}
